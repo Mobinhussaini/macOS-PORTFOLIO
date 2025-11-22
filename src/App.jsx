c6f63912a1a4af715306033f76eee5fb6a798953
Mobinhussaini <mobin.hussaini@gmail.com>
2025-11-22 15:24:14 +0430

    Welcome component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import Navbar from '#components/Navbar'
+
+import { Navbar, Welcome } from '#components';
 
 const App = () => {
   return (
     <div>
-      <Navbar /> 
+      <Navbar />
+      <Welcome /> 
       </div>
   )
 }
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Navbar from '#components/Navbar'; 
+import Welcome from '#components/Welcome';
+
+
+
+export { 
+   Navbar,
+   Welcome
+};
