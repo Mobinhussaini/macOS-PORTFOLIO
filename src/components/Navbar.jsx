de9a4ba40166e13add060ce293bf72c9ba2aed0d
Mobinhussaini <mobin.hussaini@gmail.com>
2025-11-22 15:16:34 +0430

    Navbar has been added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { navLinks, navIcons } from "#constants";
+import dayjs from 'dayjs'; 
+
+const Navbar = () => {
+   return (
+      <nav>
+         <div>
+            <img src="/images/logo.svg" alt="logo" />
+            <p className="font-bold">Mobin's Portfolio</p>
+            <ul>
+               {navLinks.map(({ id, name }) => (
+                  <li key={id}>{name}</li>
+               ))}
+            </ul>
+         </div>
+         <div>
+            <ul>
+               {navIcons.map(({ id, img }) => (
+                  <li key={id}>
+                     <img src={img} className="icon-hover" alt={`icon-${id}`} />
+                  </li>
+               ))}
+            </ul>
+            <time>{dayjs().format("ddd MMM d h:mm A")}</time>
+         </div>
+      </nav>
+   );
+};
+
+export default Navbar;
